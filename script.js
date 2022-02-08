6ede2622a7074505352414afa6f34367ad903283
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-08 12:06:11 +0600

    create find modify

---
+++ script.js
@@ -0,0 +1,8 @@
+// Method 1.
+
+// Create
+document.body.innerHTML = "<h1>Hello world!</h1>";
+// Find
+let h1Element = document.querySelector('h1');
+// Modify
+h1Element.style.color = "rgb(255, 0, 0";
