52c2b10cc5ce8ff853c8d0d802a17d23324e792b
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-09 11:30:35 +0600

    added a new elements

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-// Method 1.
+// // Method 1.
 
-// Create
-document.body.innerHTML = "<h1>Hello world!</h1>";
-// Find
-let h1Element = document.querySelector('h1');
-// Modify
-h1Element.style.color = "rgb(255, 0, 0";
+// // Create
+// document.body.innerHTML = "<h1>Hello world!</h1>";
+// // Find
+// let h1Element = document.querySelector('h1');
+// // Modify
+// h1Element.style.color = "rgb(255, 0, 0)";
+
+// // Method 2.
+
+// // Create
+// let h2Element = document.createElement('h2');
+// // Attach
+// document.body.prepend(h2Element);
+// // Modify
+// h2Element.textContent = "Nice to meet you!";
+
+let exercisesElement = document.querySelector('#exercises');
+
+// create
+let h1Element = document.createElement('h1'); 
+// modify
+h1Element.textContent = "H1 header";
+// attach
+exercisesElement.append(h1Element);
+
+
+let h2Element = document.createElement('h2');
+h2Element.textContent = "H2 header";
+exercisesElement.append(h2Element);
+
+let h3Element = document.createElement('h3');
+h3Element.textContent = "H3 header";
+exercisesElement.append(h3Element);
+
+let h4 = document.createElement('h4');
+exercisesElement.append('H4 Element');
+h4Element.textContent("header 4");
+
