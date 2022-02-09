7834b32f8ea73d1f71a8c8dcc70534ae8440a417
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-09 12:06:00 +0600

    h1-h6 acreated js

--- script.js
+++ script.js
@@ -18,6 +18,11 @@
 
 let exercisesElement = document.querySelector('#exercises');
 
+// append - insert element before closing tag
+// prepend - insert element after opening tag
+// before - insert element before opening tag
+// after - insert element after closing tag
+
 // create
 let h1Element = document.createElement('h1'); 
 // modify
@@ -34,7 +39,17 @@ let h3Element = document.createElement('h3');
 h3Element.textContent = "H3 header";
 exercisesElement.append(h3Element);
 
-let h4 = document.createElement('h4');
-exercisesElement.append('H4 Element');
-h4Element.textContent("header 4");
+let h4Element = document.createElement('h4');
+exercisesElement.append('h4Element');
+h4Element.textContent = ('H4 Header');
 
+let h5Element = document.createElement('h5');
+h5Element.textContent = "H5 Header";
+exercisesElement.before('h5Element');
+
+let h6Element = document.createElement('h6');
+h6Element.textContent = "H6 Header";
+exercisesElement.after('h6Element');
+
+
+// Exercise
